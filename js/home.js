2a1a04834d74bc5052c5a5ef402d5f14cea23e40
codesmith <student@mac-mini-9.local>
2016-08-18 00:20:10 -0700

    regex finding longer fragments

--- js/home.js
+++ js/home.js
@@ -1,65 +1,68 @@
 var rightNow = new Date();
+var quoteArray = ["Life is like a roller coaster, live it, be happy, enjoy life.", "I like people that enjoy life, 'cause I do the same.", "Simply enjoy life and the great pleasures that come with it.", "Take a step back, evaluate what is important, and enjoy life.", "Positive anything is better than negative nothing.", "Believe that life is worth living and your belief will help create the fact."]
+
+var randNum = Math.floor(Math.random()*6);
+var randomQuote = quoteArray[randNum];
 
 if(rightNow.getHours() < workStart || rightNow.getHours() > workEnd){
+	console.log("get back to work");
 	
-
-	console.log("home is so great");
+	textReplacer(randomQuote);
 }
 else {
-	var topbar = Document.createElement("div");
-	topbar.text("HAHFHAH");
-
+		console.log("home is so great");
 	
-
-
-	console.log("get back to work");
 }
+var quoteArray = ["Life is like a roller coaster, live it, be happy, enjoy life.", "I like people that enjoy life, 'cause I do the same.", "Simply enjoy life and the great pleasures that come with it.", "Take a step back, evaluate what is important, and enjoy life.", "Positive anything is better than negative nothing.", "Believe that life is worth living and your belief will help create the fact."]
+
+var randNum = Math.floor(Math.random()*6);
+var randomQuote = quoteArray[randNum];
 
 
-// var html =
-//     "<p>This is a test.</p>" +
-//     "<form><input type='text' value='test value'></form>" +
-//     "<p class='testing test'>Testing here too</p>";
-var frag = document.createDocumentFragment();
-var body = document.body;
-var node, next;
 
-// Turn the HTML string into a DOM tree
-// body.innerHTML = html;
 
-// Walk the dom looking for the given text in text nodes
-walk(body);
+function textReplacer(newtext){
+	var frag = document.createDocumentFragment();
+	var body = document.body;
+	var node, next;
 
-// Insert the result into the current document via a fragment
-node = body.firstChild;
-while (node) {
-  next = node.nextSibling;
-  frag.appendChild(node);
-  node = next;
-}
-document.body.appendChild(frag);
+	// Turn the HTML string into a DOM tree
+	// body.innerHTML = html;
 
-// Our walker function
-function walk(node) {
-  var child, next;
+	// Walk the dom looking for the given text in text nodes
+	walk(body);
 
-  switch (node.nodeType) {
-    case 1:  // Element
-    case 9:  // Document
-    case 11: // Document fragment
-      child = node.firstChild;
-      while (child) {
-        next = child.nextSibling;
-        walk(child);
-        child = next;
-      }
-      break;
-    case 3: // Text node
-      handleText(node);
-      break;
-  }
-}
+	// Insert the result into the current document via a fragment
+	node = body.firstChild;
+	while (node) {
+	  next = node.nextSibling;
+	  frag.appendChild(node);
+	  node = next;
+	}
+	document.body.appendChild(frag);
 
-function handleText(textNode) {
-  textNode.nodeValue = textNode.nodeValue.replace(/\w+\b/gi, "TEST");
+	// Our walker function
+	function walk(node) {
+	  var child, next;
+
+	  switch (node.nodeType) {
+	    case 1:  // Element
+	    case 9:  // Document
+	    case 11: // Document fragment
+	      child = node.firstChild;
+	      while (child) {
+	        next = child.nextSibling;
+	        walk(child);
+	        child = next;
+	      }
+	      break;
+	    case 3: // Text node
+	      handleText(node);
+	      break;
+	  }
+	}
+
+	function handleText(textNode) {
+	  textNode.nodeValue = textNode.nodeValue.replace(/[A-Z][^.]*\./gi, newtext);
+	}
 }
--- js/work.js
+++ js/work.js
@@ -1,13 +1,66 @@
 var rightNow = new Date();
 
+var quoteArray = ["No one ever drowned in sweat.", "I’m a greater believer in luck, and I find the harder I work the more I have of it.", "A dream doesn’t become reality through magic; it takes sweat, determination and hard work.", "Hard work keeps the wrinkles out of the mind and spirit.", "The highest reward for man’s toil is not what he gets for it, but what he becomes by it.", "Hard work helps. It has never killed anyone."]
+var randNum = Math.floor(Math.random()*6);
+var randomQuote = quoteArray[randNum];
+
 if(rightNow.getHours() > workStart && rightNow.getHours() < workEnd){
-	console.log("working hard");
-}
+	textReplacer(randomQuote);
+	
+	}
 else {
 
-	
 
-	
 
 	console.log("it's not time for working");
+}
+
+
+function textReplacer(newtext){
+	var frag = document.createDocumentFragment();
+	var body = document.body;
+	var node, next;
+
+	// Turn the HTML string into a DOM tree
+	// body.innerHTML = html;
+
+	// Walk the dom looking for the given text in text nodes
+	walk(body);
+
+	// Insert the result into the current document via a fragment
+	node = body.firstChild;
+	while (node) {
+	  next = node.nextSibling;
+	  //frag.appendChild(node);
+	  node = next;
+	}
+	document.body.appendChild(frag);
+
+	// Our walker function
+	function walk(node) {
+	  var child, next;
+
+	  switch (node.nodeType) {
+	    case 1:  // Element
+	    case 9:  // Document
+	    case 11: // Document fragment
+	      child = node.firstChild;
+	      while (child) {
+	        next = child.nextSibling;
+	        walk(child);
+	        child = next;
+	      }
+	      break;
+	    case 3: // Text node
+	      handleText(node);
+	      break;
+	  }
+	}
+
+
+
+	function handleText(textNode) {
+		
+	  textNode.nodeValue = textNode.nodeValue.replace(/[A-Z][^.]*\./gi, newtext);
+	}
 }
